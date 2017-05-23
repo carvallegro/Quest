66963c83033608610b847297c5cad3d08132145a
carvallegro <valentin.carmignac@gmail.fr>
2017-05-23 11:03:48 +1000

    Improved Menu and Map

--- src/modules/Map/Map.js
+++ src/modules/Map/Map.js
@@ -25,21 +25,21 @@ class Map extends Component {
     initMap() {
         const map = GoogleMap.Native.Map(this.refs.map, {
             center: {lat: 61.769256, lng: 92.111992},
-            // center: {lat: -24.350646, lng: 133.836676},
             zoom: 3,
             minZoom: 3,
-            // disableDefaultUI: true,
-            // disableDoubleClickZoom: true,
-            // draggable: false,
-            // fullscreenControl: false,
-            // gestureHandling: 'none',
-            // keyboardShortcuts: false,
-            // mapTypeControl: false,
-            // panControl: false,
-            // rotateControl: false,
-            // scaleControl: false,
-            // scrollwheel: false,
-            // zoomControl: false,
+            disableDefaultUI: true,
+            disableDoubleClickZoom: true,
+            draggable: false,
+            fullscreenControl: false,
+            gestureHandling: 'none',
+            keyboardShortcuts: false,
+            mapTypeControl: false,
+            panControl: false,
+            rotateControl: false,
+            scaleControl: false,
+            scrollwheel: false,
+            zoomControl: false,
+            backgroundColor: '#0D252E',
             styles: googleMapStyle
         });
 
@@ -60,15 +60,32 @@ class Map extends Component {
         })
     }
 
+    postToDisplay() {
+        if (!this.props.mapProperties.post) {
+            return null;
+        }
+
+        let post = this.props.mapProperties.post;
+
+        // TODO change the center feature
+        this.state.map.panTo(post.place.position);
+        return <PlaceMarker map={this.state.map}
+                            pos={post.place.position}
+                            type={post.place.pinType}
+                            onClick={() =>
+                                this.props.displayPostContent(post)
+                            }/>;
+    }
+
     render() {
         return (
             <div className="mapContainer">
                 <div ref="map" className="map"></div>
-                <TextOverlay pos={{lat: 64.642520, lng: 87.092891}} text="Russie" textSize={10} map={this.state.map}/>
-                <TextOverlay pos={{lat: -23, lng: 133.919558}} text="Australie" textSize={5} map={this.state.map}/>
                 <TextOverlay pos={{lat: 48.859529, lng: 2.343104}} text="Paris" textSize={1} map={this.state.map}/>
+                <TextOverlay pos={{lat: 62.642520, lng: 87.092891}} text="Russie" textSize={10} map={this.state.map}/>
+                <TextOverlay pos={{lat: 24.269321, lng: 113.158083}} text="Honk Kong" textSize={1} map={this.state.map}/>
+                <TextOverlay pos={{lat: -23, lng: 133.919558}} text="Australie" textSize={5} map={this.state.map}/>
                 { this.renderChildren() }
-                <PlaceMarker pos={{lat: 64.642520, lng: 87.092891}} map={this.state.map} type={PinTypes.PLACEHOLDERS}/>
 
                 {/*Paris Londres*/}
                 <MapLine pos={{lat: 48.859529, lng: 2.343104}} toPos={{lat: 51.508172, lng: -0.126679}}
@@ -79,9 +96,16 @@ class Map extends Component {
                 {/*Honk Kong Melbourne*/}
                 <MapLine pos={{lat: 22.269321, lng: 114.158083}} toPos={{lat: -37.816977, lng: 144.965170}}
                          map={this.state.map}/>
+
+                {this.postToDisplay()}
             </div>
         )
     }
 }
+
+Map.propTypes = {
+    mapProperties: PropTypes.object.isRequired,
+    displayPostContent: PropTypes.func.isRequired
+};
 
 export default Map;
--- src/modules/Menu/Menu.js
+++ src/modules/Menu/Menu.js
@@ -9,20 +9,33 @@ class Menu extends Component {
         let menuOuterClasses = 'menu-outter ' + (this.props.isMenuOpen ? 'open' : '');
         return (
             <div className={menuOuterClasses}>
-                <a className="menu-trigger" onClick={this.props.toggleMenu} href="#!">
-                    <i className="fi flaticon-menu"></i>
-                </a>
-                <div className="menu-inner">
-                    <ul className="menu-content">
-                        <li><a onClick={this.props.toggleMenu}><i className="fi flaticon-error"></i> Close</a></li>
-                        <li>menu : {this.props.isMenuOpen ? 'true' : 'false'}</li>
-                        <li>Length : {this.props.posts.length}</li>
+                <div className="menu-header">
+                    <ul className="menu-header-actions">
+                        <li className="first"><a className="menu-trigger" onClick={this.props.toggleMenu} href="#!">
+                            <i className="fi flaticon-calendar"></i>
+                        </a></li>
+                        <li className="second"><a className="menu-trigger" onClick={this.props.toggleMenu} href="#!">
+                            <i className="fi flaticon-map-location"></i>
+                        </a></li>
+                        <li className="third"><a className="menu-trigger" onClick={this.props.toggleMenu} href="#!">
+                            <i className="fi flaticon-idea"></i>
+                        </a></li>
                     </ul>
+
+                </div>
+                <div className="menu-inner">
                     <p>Places</p>
                     <ul className="menu-content">
-                        {this.props.posts.map(post => {
-                            return <li>{post.id} - {post.title}</li>
-                        })}
+                        {this.props.posts.map(post =>
+                            <li key={post.id}
+                                onClick={() => this.props.setMapPost(post)}
+                                onDoubleClick={() => this.props.displayPostContent(post)}>
+                                {post.id} - {post.title} - {post.isContentVisible ? 'true' : 'false'}
+                            </li>
+                        )}
+                        <li onClick={() => this.props.setMapPost(null)}>
+                            NULL
+                        </li>
                     </ul>
                 </div>
             </div>
@@ -33,7 +46,9 @@ class Menu extends Component {
 
 Menu.propTypes = {
     isMenuOpen: PropTypes.bool.isRequired,
-    toggleMenu: PropTypes.func.isRequired
+    toggleMenu: PropTypes.func.isRequired,
+    setMapPost: PropTypes.func.isRequired,
+    displayPostContent: PropTypes.func.isRequired,
 };
 
 
