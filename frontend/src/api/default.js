6da64b437a6ff8bc3f868e8de961b5c332ce7d5a
carvallegro <valentin.carmignac@gmail.fr>
2017-06-01 13:41:50 +1000

    Fixed the CORS situation and added config management to the backend

--- frontend/src/api/default.js
+++ frontend/src/api/default.js
@@ -1,14 +1,9 @@
 export default class defaultApi {
     static BASE_URL = process.env.REACT_APP_API_HOST + '/api';
 
-    // TODO : fix this CORS situationgit
+    // TODO : fix this CORS situation
     static DEFAULT_FETCH_OPTIONS = {
-        method: 'GET',
-        mode: 'cors',
-        header: {
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
-        }
+        method: 'GET'
     };
 
     static url() {
--- server/src/app.js
+++ server/src/app.js
@@ -1,22 +1,31 @@
 import express from 'express'
+import config from 'config'
 import cors from 'cors'
 import http from 'http'
 import post from './routes/post'
 import textOverlay from './routes/text_overlay'
 
-const app = express();
-app.use(cors());
+console.log('NODE_ENV: ' + config.util.getEnv('NODE_ENV'));
+console.log(config.server.port)
 
-app.get('/', (req, res) => res.send('Hello World ! Welcome to Quest backend :)'));
+const app = express();
+
+var corsOptions = {
+    origin: config.front.url
+};
+app.use(cors(corsOptions));
+
+app.get('/', (req, res) => res.send('Hello World ! Welcome to the  Quest backend :)'));
 
 // Routes settings
 const apiRoute = express.Router();
+
 apiRoute.use('/post', post);
 apiRoute.use('/text-overlay', textOverlay);
 
 app.use('/api', apiRoute);
 
 let server = http.createServer(app);
-server.listen(3100, () => console.log('Quest backend app listening on port 3100!'));
+server.listen(config.server.port, () => console.log('Quest backend app listening on port ' + config.server.port));
 
 export default app;
